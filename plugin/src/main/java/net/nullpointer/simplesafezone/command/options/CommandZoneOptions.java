f7d5e01d138e84047a435d6f7981b2e7d8849534
SQuazar <flawesv@yandex.ru>
2025-02-12 15:42:10 +0300

    Moved to options package

--- plugin/src/main/java/net/nullpointer/simplesafezone/command/options/CommandZoneOptions.java
+++ plugin/src/main/java/net/nullpointer/simplesafezone/command/options/CommandZoneOptions.java
@@ -1,28 +1,22 @@
-package net.nullpointer.simplesafezone.command;
+package net.nullpointer.simplesafezone.command.options;
 
 import net.nullpointer.simplesafezone.SimpleSafeZone;
-import net.nullpointer.simplesafezone.command.options.CommandOptionRule;
+import net.nullpointer.simplesafezone.command.CommandBase;
+import net.nullpointer.simplesafezone.command.CommandBaseZone;
 import net.nullpointer.simplesafezone.util.Messages;
 import net.nullpointer.simplesafezone.util.Permissions;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import static net.nullpointer.simplesafezone.util.ColorUtils.colorize;
 
-public class CommandZoneOptions extends CommandBase {
+public class CommandZoneOptions extends CommandBaseZone {
     public CommandZoneOptions(SimpleSafeZone plugin, CommandBase parent) {
-        super(plugin, parent, "option", "Safe zone options", Permissions.SAFE_ZONE_OPTIONS, true);
+        super(plugin, parent, "option", "Safe zone options", Permissions.SAFE_ZONE_OPTIONS);
         registerSubCommand("rule", new CommandOptionRule(plugin, this));
     }
 
     @Override
     public void onCommand(CommandSender sender, String[] args) {
         sender.sendMessage(colorize(plugin.getMessages().getString(Messages.ENTER_OPTION.getKey())));
-    }
-
-    @Override
-    public boolean canTabComplete(CommandSender sender) {
-        Player player = (Player) sender;
-        return plugin.getSafeZoneManager().findSafeZone(player.getUniqueId()).isPresent();
     }
 }
