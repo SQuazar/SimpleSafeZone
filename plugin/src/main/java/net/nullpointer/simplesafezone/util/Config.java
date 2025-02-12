33587367e75558252ea3498d940e9a920319c339
SQuazar <flawesv@yandex.ru>
2025-02-12 15:40:32 +0300

    Added config property allowed-worlds

--- plugin/src/main/java/net/nullpointer/simplesafezone/util/Config.java
+++ plugin/src/main/java/net/nullpointer/simplesafezone/util/Config.java
@@ -4,6 +4,7 @@ public enum Config {
     AUTO_CUI("auto-cui"),
     LIFE_TIME("life-time"),
     STORE_ZONES("store-zones"),
+    ALLOWED_WORLDS("allowed-worlds"),
     SAFE_ZONE_DEFAULT_RADIUS("safezone.default-radius"),
     SAFE_ZONE_MAX_RADIUS("safezone.max-radius"),
     SAFE_ZONE_MIN_RADIUS("safezone.min-radius"),
